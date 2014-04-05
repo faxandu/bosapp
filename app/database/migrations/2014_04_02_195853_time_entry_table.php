74abfeaafd1cfa86f1a6ad0a9a7d3d7df8041ca5
b-dub25 <brundel25@gmail.com>
2014-04-04 22:35:16 -0400

    bring up of pivot tables and other stuff

---
+++ app/database/migrations/2014_04_02_195853_time_entry_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TimeEntryTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('time_tracking_entry',function($table) {
+
+            $table->increments('id');
+            $table->date('startDate');
+            $table->date('endDate')->nullable();
+            $table->time('startTime');
+            $table->time('endTime')->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('clock_in')->defalut(false);
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+
+	public function down()
+	{
+		Schema::drop('time_entry_table');
+	}
+
+}
--- app/database/migrations/2014_04_05_015828_time_tracking_categories_table.php
+++ app/database/migrations/2014_04_05_015828_time_tracking_categories_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CategoriesTable extends Migration {
+class TimeTrackingCategoriesTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,8 @@ class CategoriesTable extends Migration {
 	 */
 	public function up()
 	{
-        Schema::create('time_categories_table', function($table){
+        Schema::create('time_tracking_categories',function($table){
+
             $table->increments('id');
             $table->string('category',30);
         });
@@ -25,7 +26,7 @@ class CategoriesTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('time_categories_table');
+		Schema::drop('time_tracking_categories');
 	}
 
 }
