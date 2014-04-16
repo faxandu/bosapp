7fa309c153eb8e18576bca823eba0e27bb2ceacf
Craig <craig@mint-test.com>
2014-04-16 17:11:22 -0400

    Fixed old file issues

--- app/views/userStaff.php
+++
@@ -1,9 +0,0 @@
-<html>
-	<form action="lotto/setUserStaffType" method= 'POST'>
-		<table>
-			<tr><td> user: </td><td> <input type="text" name = "user" > </td></tr>
-			<tr><td> staff: </td><td> <input type="text" name = "staffType" > </td></tr>
-		</table>
-		<input type="submit" value="submit">
-	</form>
-</html>
