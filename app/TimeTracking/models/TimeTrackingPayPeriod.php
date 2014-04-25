fecc25fcd74e85b2813db0aac9521c7b354b4d09
b-dub25 <brundel25@gmail.com>
2014-04-25 13:11:54 -0400

    fixed some syntax bring up of pay period controller

--- app/TimeTracking/models/TimeTrackingPayPeriod.php
+++ app/TimeTracking/models/TimeTrackingPayPeriod.php
@@ -20,4 +20,15 @@ class TimeTrackingPayPeriod extends Eloquent{
         return $this->select(array('start_pay_period','end_pay_period'));
     }
 
+    private static $rules = array(
+
+    	'start_pay_period' => 'unique:start_pay_period',
+    	'end_pay_period'   => 'unique:end_pay_period' ,
+    	'start_pay_period' => 'before:end_pay_period' ,
+    	'end_pay_period'   => 'after:start_pay_period',
+    	);
+    
+    public static function validate($pay_period){
+    	return Validaton::make($pay_period, static::$rules);
+    }
 } 
