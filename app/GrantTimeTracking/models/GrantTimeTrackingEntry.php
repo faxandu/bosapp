b7053f7583f5d57e2bd283c7fb646ddc5eb65ddd
b-dub25 <brundel25@gmail.com>
2014-04-26 14:48:57 -0400

    bring up of grants time tracking models

---
+++ app/GrantTimeTracking/models/GrantTimeTrackingEntry.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brian
+ * Date: 4/5/14
+ * Time: 12:57 PM
+ */
+
+namespace TimeTracking\models;
+use Eloquent, Validaton, Exception;
+
+class GrantTimeTrackingEntry extends Eloquent{
+
+    private $table = 'grant_time_tracking_entry';
+    private $fillable = array('start_time','end_time','start_date'
+                             ,'end_date','description','clocked_in','category'
+                             ,'user_id','category_id','pay_id');
+    private $guarded = 'id';
+    private $timestamps = false;
+
+    public function entry(){
+        return $this->belongsTo('user');
+    }
+
+} 
