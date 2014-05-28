d45a4e1dde48d27af156c503ea3ce51556877537
b-dub25 <brundel25@gmail.com>
2014-05-28 14:35:17 -0400

    this is a commit

--- app/TimeTracking/controllers/TimeTrackingCategories.php
+++ app/TimeTracking/controllers/TimeTrackingCategories.php
@@ -4,6 +4,11 @@
  * User: brian
  * Date: 4/18/14
  * Time: 10:25 PM
+ * This is the implementation for the model @see Categories.php
+ * it makes it possible to create new catagories to be stored 
+ * into the database.  
+ * private functions will be located at the bottom.
+ * 
  */
 
 namespace TimeTracking\controllers;
@@ -15,13 +20,20 @@ use TimeTracking\models\Categories;
 
 class TimeTrackingCategories extends BaseController{
 
-
+    /**
+    * This function is used to add a category to the table. 
+    * It will get input from the user and validate it from  
+    * through a helper function ( @see failed($category) )
+    * if the category is found to be unique it will 
+    * then create an new object of Categories . 
+    * If the object can not be saved for whatever reason it 
+    * @throws exception 
+    */
     public function postAddCategory(){
 
         $input = Input::all();
-        $temp = Categories::validate($input['category'])
-
-        if(!$temp->fails()){
+    
+        if(!$this->failed($input)){
 
             $category = new Categories();
             try
@@ -41,10 +53,16 @@ class TimeTrackingCategories extends BaseController{
                 array('status' => 401, 'message' => 'category already exists '), 401);
     }
 
+    /**
+    * This function will delete a category based upon a 
+    * the id sent in from the user. If the category does not
+    * exist it @throws exception 
+    * 
+    */
     public public function postDeleteCategory(){
         
-        $input = Input::all();
-        $category = Categories::find($input['id']);
+        
+        $category = Categories::find(Input::get('id' ) );
         try {
              $category->delete();
              Response::json(
@@ -83,7 +101,10 @@ class TimeTrackingCategories extends BaseController{
             Response::json(
                 array('status'=> 401 , 'message' => 'category does not exist'), 401);
     }
-
+    /**
+    * @link http://laravel.com/docs/validation 
+    * Documentation for fails() function  
+    */
     private function failed($category){
         return Categories::validate($category)->fails();
     }
--- app/TimeTracking/controllers/TimeTrackingPayPeriod.php
+++ app/TimeTracking/controllers/TimeTrackingPayPeriod.php
@@ -4,6 +4,8 @@
  * User: brian
  * Date: 4/18/14
  * Time: 11:04 PM
+ * This class is the implementation of the model @link{#/app/TimeTracking/models/TimeTrackingPayPeriod.php } 
+ *  
  */
 
 namespace TimeTracking\controllers;
@@ -12,15 +14,24 @@ use BaseController, User,  Entry ,Response;
 
 class TimeTrackingPayPeriod extends BaseController{
 
-
+    /**
+    * This function is used to create a new payperiod 
+    * to be saved to the payperiod table. This function 
+    * makes a call to the the helper function and @uses postPayPeriod()
+    * create a @see TimeTrackingPayPeriod object and pass it to said function
+    */
 	public function postCreatPayPeriod(){
    
     $pay_period = new TimeTrackingPayPeriod();
     $this->postPayPeriod($pay_period);
       
 	}
-
-	public function postDeletePeriod(){
+    /**
+    * This function @uses TimeTrackingPayPeriod::find(Input::get('id'))
+    * to find the given payperiod object if it exists. If it doesn't 
+    * it , or if the id was not found it @throws an exception.   
+    */
+	public function postDeletePeriod() {
 
 		$period = TimeTrackingPayPeriod::find(Input::get('id'));
 		try
@@ -34,21 +45,35 @@ class TimeTrackingPayPeriod extends BaseController{
 		}
 
 	}
-    
+    /**
+    * This function is like , @see postCreatPayPeriod(), but it will  
+    * not create a new object of TimeTrackingPayPeriod
+    * and instead will find the payperiod by id number and 
+    * @uses TimeTrackingPayPeriod::find() to do so. It will pass 
+    * that object to the function @uses postPayPeriod($pay_period )
+    * for more information on that function please @see postPayPeriod($pay_period)
+    */
 	public function postModifyPeriod(){
 
 		$period = TimeTrackingPayPeriod::find(Input::get('id'));
 		$this->postPayPeriod($period);
 
 	}
-    
+    /**
+    * This function is a helper function, @see postModifyPeriod()
+    * and @see postCreatPayPeriod(). It will validate time and  
+    * make sure the payperiod is unique to avoid duplicate dates.
+    * If the date is valid it will store the start date and end in the 
+    * @param object that is being passed into the function at call .
+    * If the object can not be save it @throws an exception       
+    */
     private function postPayPeriod($pay_period){
 
 		if(!$this->failed(Input::get('start_pay_period')) 
     		&& !$this->failed(Input::get('end_pay_period'))){
     		$start_date = new date("Y-n-j",strtotime(Input::get('start_pay_period')));
     		$end_date = new date("Y-n-j",strtotime(Input::get('end_pay_period')));
-    	
+    	    
     		try
     		{
     			$pay_period['start_pay_period'] = $start_date;
